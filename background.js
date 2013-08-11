65f49cfa037d93e16bdd3af50422ddf6495e2169
Chris Lamb <lamby@debian.org>
2013-08-11 18:42:15 +0100

    Strava have moved to www.strava.com

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (tab.url.indexOf('app.strava.com') > -1) {
+  if (tab.url.indexOf('www.strava.com') > -1) {
     chrome.pageAction.show(tabId);
   }
 });
